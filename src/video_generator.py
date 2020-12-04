d977221b8141f02589b207548a28f01745ab6713
João Agnelo <joaoa13portugal@gmail.com>
2020-12-04 04:24:43 +0000

    Update video_generator.py

--- src/video_generator.py
+++ src/video_generator.py
@@ -40,7 +40,7 @@ def generate_video_from_targz(targz_file_name):
         dst_dir = os.path.join(dst_dir, Globals.SNAPSHOTS_DIR)
     snapshots_path = os.path.join(dst_dir, Globals.SNAPSHOTS_FILE_NAME)
     video_path = os.path.join(Globals.VIDEOS_DIR, file_name)
-    cmd = 'ffmpeg -y -r 30 -i "snapshot%d.png" -vcodec libx264 -crf 28 -pix_fmt yuv420p "../videos/%s.mp4"' % file_name
+    cmd = 'ffmpeg -y -r 30 -i "snapshot%s.png" -vcodec libx264 -crf 28 -pix_fmt yuv420p "../videos/%s.mp4"' % ("%d", file_name)
     print("CMD", cmd.split(" "))
     proc = subprocess.Popen(cmd.split(" "), stdout=PIPE, stdin=PIPE, stderr=PIPE, cwd=dst_dir)
     stdout, stderr = proc.communicate()
