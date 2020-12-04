fae9955ec100467b104c0e5916392e1401a0bd14
João Agnelo <joaoa13portugal@gmail.com>
2020-12-04 01:47:52 +0000

    small fixes

--- src/video_generator.py
+++ src/video_generator.py
@@ -24,13 +24,14 @@ def generate_video_from_targz(targz_file_name):
     utils.unzip_targz(src_dir, dst_dir)
     if os.path.exists(os.path.join(dst_dir, Globals.SNAPSHOTS_DIR)):
         dst_dir = os.path.join(dst_dir, Globals.SNAPSHOTS_DIR)
-    video_name = "%s.%s" % (file_name, Globals.VIDEOS_FILE_TYPE)
     snapshots_path = os.path.join(dst_dir, Globals.SNAPSHOTS_FILE_NAME)
-    video_path = os.path.join(Globals.VIDEOS_DIR, video_name)
+    video_path = os.path.join(Globals.VIDEOS_DIR, file_name)
     cmd = Globals.FFMPEG_CMD % (snapshots_path, video_path)
+    p = None
     try:
-        subprocess.run(cmd.split(" "), check=True)
-        print("SUCCESS")
+        p = subprocess.run(cmd.split(" "), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        print(p.stdout.decode().rstrip())
     except subprocess.CalledProcessError as e:
         print("ERROR: ", e)
-
+        if p:
+            print(print(p.stderr.decode().rstrip()))
